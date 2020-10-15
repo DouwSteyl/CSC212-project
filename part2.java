1f9ddf9de22d88258de4a613d67acbf1d0a173a4
Douw Steyl <douwjnr@gmail.com>
2020-10-15 11:58:50 +0200

    added csv and template file

---
+++ part2.java
@@ -0,0 +1,47 @@
+public class part2{
+
+    public class group{
+
+        private String name;
+        private int members;
+        private int weight;
+
+        public group(String n, int m, int w){
+            this.name = n;
+            this.members = m;
+            this.weight = w;
+        }    
+
+
+        //gets
+        public String getName(){return name;}
+        public int getMembers(){return members;}
+        public int getWeight(){return weight;}
+
+        public String toString(){
+            return "Group "+name+": "+members+" members, with a weight of "+weight+"\n";
+        }
+
+    }
+
+
+    public static 
+
+
+
+
+
+
+
+
+
+
+    public static void main(String args[]){
+
+
+
+
+
+    }
+
+}
