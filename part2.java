fefc17385ccc09c2e90b2238ef96508af932c7da
Douw Steyl <douwjnr@gmail.com>
2020-10-19 20:43:07 +0200

    next to final code

--- part2.java
+++ part2.java
@@ -8,125 +8,147 @@ import java.util.List;
 
 public class part2 {
 
-    public int cap = 100;
-    public int[][] matrix;
-    public List<group> Groups;
-    // constructor to initialize arraylist and fill it
+    public int cap = 100;       // maximum capacity of the busses
+    public int[][] matrix;      // matrix used to evaluate which way way of filling the busses would be optimal
+    public static List<group> Groups; // using Lists/arraylist of group objects, to simplify the process of iteration
+                                      // and evaluation
 
-    public part2(int cap, String[] groupnames, int[] amountmembers, int[] weight) {
-        this.cap = cap;
+    // constructor to initialize arraylist and fill it with the different group
+    // objects
+    public part2( String[] groupnames, int[] amountmembers, int[] weight) {
+        this.cap = 100;
         this.Groups = new ArrayList<group>(groupnames.length);
-        for(int x = 0; x < groupnames.length;x++){
+        for (int x = 0; x < groupnames.length; x++) {
             Groups.add(new group(groupnames[x], amountmembers[x], weight[x]));
         }
     }
 
+    // constructor for group objects
+    public class group {
 
-    public class group{
+        private String name;
+        private int members;
+        private int weight;
+        private int profits;
 
-        String name;
-        int members;
-        int weight;
-        int profits;
-
-        public group(String n, int w, int m){
+        // constructor for initializing the group object, adding all relevant info, as
+        // well as a field for profits to be used later on
+        public group(String n, int w, int m) {
             this.name = n;
             this.members = m;
             this.weight = w;
-            if(this.members > 100){
-                this.profits = (members- 100)*5;
-            }else{
+            if (this.members > 100) { // services only charge after the first 100kg, so just a condition to ensure
+                                      // there are no non-positive profits
+                this.profits = (members - 100) * 5;
+            } else {
                 this.profits = 0;
             }
-        }    
+        }
 
+        // gets/accessor methods
+        public String getName() {return name;}
 
-        //gets
-        public String getName(){return name;}
-        public int getMembers(){return members;}
-        public int getWeight(){return weight;}
-        public int getProfits(){return profits;}
+        public int getMembers() {return members;}
 
-        public String toString(){
-            return "Group "+name+": "+weight+" members, with a weight of "+members;
+        public int getWeight() {return weight;}
+
+        public int getProfits() {return profits;}
+
+        // basic to string for neater printing, taking into consideration the switching
+        // of members and their weights!!!!!
+        public String toString() {
+            return "Group " + name + ": " + weight + " members, with a weight of " + members+"kg.";
         }
 
     }
 
+    // method for determining the optimal way to pack/organize the busses
+    public int knapsack() {
 
-    public int knapsack(){
-        
-        this.matrix = new int[Groups.size()+1][cap+1];
-        for(int i=0; i <= Groups.size();i++ ){
-            for (int j=0; j <= cap;j++){
-                if( i==0 || j==0 ){
-                    matrix[i][j] = 0;
-                }else if ( Groups.get(i-1).weight <= j ){
-                    matrix[i][j] = Math.max( Groups.get(i-1).members + matrix[i-1][j - Groups.get(i-1).weight] , matrix[i-1][j] );
-                }else{
-                    matrix[i][j] = matrix[i-1][j];
+        this.matrix = new int[Groups.size() + 1][cap + 1];
+
+        for (int x = 0; x <= Groups.size(); x++) { // iterates the amount of times so that each group is covered
+            for (int y = 0; y <= cap; y++) { // iterates until the maximum capacity of the busses are reached
+                if (x == 0 || y == 0) {
+                    matrix[x][y] = 0;
+                } else if (Groups.get(x - 1).weight <= y) { // condition to determine max 
+                    matrix[x][y] = Math.max(Groups.get(x - 1).members + matrix[x - 1][y - Groups.get(x - 1).weight],
+                            matrix[x - 1][y]);
+                } else {
+                    matrix[x][y] = matrix[x - 1][y];
                 }
             }
-        }    
+        }
 
-        return matrix[Groups.size()][cap];
+        return matrix[Groups.size()][cap]; // returns the optimal/maximized values for the weighting
     }
 
-    public void removeGroups(){
-        
-        for(int x = 1;x <= 5;x++){
-            int index = 0;
-            int finalProf = 0;
-            int finalWeight = 0;
-            int removeable[] = new int[Groups.size()];
-            
-            this.knapsack();
+    // method used to remove schools from the list after being considered
+    public void removeGroups() {
+        int totalProf=0;            // variables for tracking the total amounts
+        int totalWeight = 0;
 
-            int w = cap;
-            String removedGroups = "";
+        for (int x = 1; x <= 5; x++) {      // only iterating 5 times, because there are only 5 busses
+            String removedGroups = "";      // variable used to accumulate all the schools being considered per iteration
+            int index = 0;                  // variable used to remember the index of schools to be removed
+            int removeable[] = new int[Groups.size()];      //  list used to store the groups to be removed
 
-            for(int i = matrix.length -1 ;i > 0 ;i--){
-                int matSingle  = this.matrix[i][w];
-                if(matSingle != this.matrix[i-1][w] ){
-                    removedGroups += Groups.get(i-1).name+" ";
+            int finalProf = 0;              // variable used to accumulate all the profits
+            int finalWeight = 0;            // variable used to accumulate all the weights
 
-                    finalWeight += Groups.get(i-1).members;
-                    finalProf += Groups.get(i-1).profits;
-                    w -= Groups.get(i-1).weight;
-                    removeable[index++] = i-1;
+
+            this.knapsack();                // calls knapsack for each given matrices, remembering that the matrices changes after each iteration
+
+            int w = cap;                    // use this variable in order to be able to iterate 
+
+            for (int y = matrix.length - 1; y > 0; y--) {
+                int matSingle = this.matrix[y][w];
+                if (matSingle != this.matrix[y - 1][w]) {
+                    finalWeight += Groups.get(y - 1).members;       // remembering that the weight and values are switched for this problem
+                    removedGroups += Groups.get(y - 1).name + " ";
+                    finalProf += Groups.get(y - 1).profits;
+
+
+                    w -= Groups.get(y - 1).weight;
+                    removeable[index++] = y - 1;        // remembers the index of the group to be removed
                 }
-
             }
-            System.out.println("Bus "+x+" has a weight of: "+finalWeight+"kg and thus a profit of: R"+finalProf+"\nThe included groups on bus "+ x+" are: "+removedGroups);
-            finalWeight = 0;
+            totalProf += finalProf;
+            totalWeight += finalWeight;
+
+            System.out.println("Bus " + x + " has a weight of: " + finalWeight + "kg and thus a profit of: R"
+                    + finalProf + "\nThe included groups on bus " + x + " are: " + removedGroups);
+            
+            finalWeight = 0;        // clear/reset these variables, so that they are accurate for next iterations
             removedGroups = "";
             finalProf = 0;
-            for(int y=0; y < index;y++){
-                Groups.remove(removeable[y]);
+
+            for (int z = 0; z < index; z++) {       // loop to iterate 5 times to remove the schools
+                Groups.remove(removeable[z]);
             }
         }
 
+        System.out.println("\nThe total profit made by the 5 busses is :R"+totalProf+", carrying a total of "+totalWeight+"kg.");
+
     }
-
-
 
     public static void main(String args[]) {
 
-        //readData();
+        String groups[] = new String[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O" };
+        int amountmembers[] = new int[] { 48, 30, 42, 36, 36, 48, 42, 42, 36, 24, 30, 30, 42, 36, 36 };
+        int weight[] = new int[] { 100, 300, 250, 500, 350, 300, 150, 400, 300, 350, 450, 100, 200, 300, 250 };
 
-        String groups[] = new String[]{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"};
-        int amountmembers[] = new int[]{48,30,42,36,36,48,42,42,36,24,30,30,42,36,36};
-        int weight[] = new int[]{100,300,250,500,350,300,150,400,300,350,450,100,200,300,250};
+        // feed the arrays of data into the constructor so it will be put into the list
+        part2 finalObject = new part2( groups, amountmembers, weight);
 
-        //knapsack kp = new knapsack(Groups, 100);
-        //knapsack.display();
+        System.out.println("To follow are the values listed in the List: ");
+        for (int x = 0; x < groups.length; x++)
+            System.out.println(Groups.get(x).toString());
 
-        //Solution sol = kp.solve();
-        //System.out.println(sol.items.get(0));
-        //sol.display();
+        System.out.println("\nTo follow are the values that were chosen by the knapsack algorithm, maximizing the weight per bus, while keeping in mind the passenger constraint per bus:");    
 
-        part2 p = new part2(100,groups,amountmembers,weight);
-        p.removeGroups();
+        // method calls knapsack within, also removes a group from the list after used, in order not to have duplicate values appearing
+        finalObject.removeGroups();
         
 
     }
