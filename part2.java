b67952cb87852c7fccddc8bbff1e8674882db093
Douw Steyl <douwjnr@gmail.com>
2020-10-19 18:05:52 +0200

    working correctly, need to edit further

--- part2.java
+++ part2.java
@@ -53,103 +53,6 @@ public class part2 {
 
     }
 
-    /////////////////////////
-    //  solution class  ////
-
-    /*public static class Solution{
-        
-        public List<group> items;
-        public int value;
-
-        public Solution(List<group> items, int value){
-            this.items = items;
-            this.value = value;
-        }
-
-        public void display() {
-            if (items != null  &&  !items.isEmpty()){
-                System.out.println("\nKnapsack solution");
-                System.out.println("Value = " + value);
-                System.out.println("Items to pick :");
-                
-                for (group item : items) {
-                    System.out.println("- " + item.toString());
-                }
-            }
-        }
-
-    }*/
-///////////////////////////
-/*
-    public static class knapsack{
-        
-        private static List<group> items;
-        private static int capacity;
-
-        public knapsack(List<group> items, int capacity) {
-            part2.knapsack.items = items;
-            part2.knapsack.capacity = capacity;
-        }
-
-        public static void display() {
-            if (items != null && items.size() > 0) {
-                System.out.println("Knapsack problem");
-                System.out.println("Capacity : " + capacity);
-                System.out.println("Items :");
-
-                for (part2.group item : items) {
-                    System.out.println("- " + item.toString());
-                }
-            }
-        }
-
-        public Solution solve() {
-            int nitems = items.size();
-            System.out.println(nitems);
-            int[][] matrix = new int[nitems + 1][capacity + 1];
-
-            for (int i = 0; i <= capacity; i++)
-                matrix[0][i] = 0;
-
-            for (int i = 1; i <= nitems; i++) {
-                for (int j = 0; j <= capacity; j++) {
-                    if (items.get(i - 1).weight > j) {
-                        matrix[i][j] = matrix[i - 1][j];
-                    } else {
-                        matrix[i][j] = Math.max(matrix[i - 1][j],
-                                matrix[i - 1][j - items.get(i - 1).weight] + items.get(i - 1).members);
-                    }
-                }
-            }
-
-            int res = matrix[nitems][capacity];
-            int w = capacity;
-            List<group> oplossing = new ArrayList<>();
-
-            for (int i = nitems; i > 0 && res > 0; i--) {
-                if (res != matrix[i - 1][w]) {
-                    oplossing.add(items.get(i - 1));
-                    res -= items.get(i - 1).members;
-                    w -= items.get(i - 1).weight;
-                }
-            }
-
-            return new Solution(oplossing, matrix[nitems][capacity]);
-        }
-
-        public void remove(Solution sol, ArrayList<group> Groups){
-
-            
-
-        }
-
-
-
-    }
-    */
-
-    ///////////////////////////
-
 
     public int knapsack(){
         
@@ -206,34 +109,6 @@ public class part2 {
     }
 
 
-    // making a list of all the groups
-    
-
-    // method of populating the empty list with data from csv
-   /* public static List<group> readData() {
-
-        BufferedReader bf = null;
-        String line = "";
-        group nuut = null;
-
-        try {
-            bf = new BufferedReader(new FileReader("data.csv"));
-            line = bf.readLine();
-            while ((line = bf.readLine()) != null) {
-                String split[] = line.split(",");
-
-                nuut = new group(split[0], Integer.parseInt(split[2]), Integer.parseInt(split[1]));
-                //System.out.println(nuut.toString());
-                Groups.add(nuut);
-            }
-
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        return Groups;
-    }*/
 
     public static void main(String args[]) {
 
